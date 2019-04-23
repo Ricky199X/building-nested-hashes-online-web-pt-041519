18c4508d493546a5f25741539d1a7cb71e687f17
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-23 14:34:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,36 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {
+        name: "Lord Monatague",
+        age: "53"
+      },
+      :matriarch => {
+        name: "Lady Montague",
+        age: "54"
+      },
+      :hero => {
+        name: "Romeo",
+        age: "15",
+        status: "Alive"
+      },
+      :heros_friends => [
+      {
+        name: "Benvolio", 
+        age: "17", 
+        attitude: "worried"
+      },
+      {
+        name: "Mercutio", 
+        age: "18", 
+        attitude: "hot-headed"
+      }
+    ]
+  }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      }
+    
+  }
 
-  
 end
