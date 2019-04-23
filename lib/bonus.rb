30ee17f4cf7bb6ac0c1f36c5a45b03b97fa901e2
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-23 14:57:48 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,10 @@ def bonus
 
   #code your solution here:
 
-  epic_tragedy[:montague][:hero]["status"] = "dead"
-  epic_tragedy[:capulet][:heroine]["status"] = "dead"
+  epic_tragedy.each do |family, data|
+    data.each do |role|
+      if role == :
+    
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
