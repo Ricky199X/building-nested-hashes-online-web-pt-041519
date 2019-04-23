6ced620ad4eaab0a449d941360c1ae69d3d87e43
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-23 14:36:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -28,9 +28,32 @@ def second_challenge
     ]
   }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {
+        name: "Lord Capulet",
+        age: "50"
+    },
+      :matriarch => {
+        name: "Lady Capulet",
+        age: "51"
+    },
+      :heroine => {
+      name: "Juliet",
+      age: "15",
+      status: "Alive"
+    },
+      :heroines_friends => [
+      {
+        name: "Steven",
+        age: "30",
+        attitude: "confused"
+      }, 
+      { 
+        name: "Nurse",
+        age: "44",
+        attitude: "worried"
       }
-    
+    ]
   }
+}
 
 end
