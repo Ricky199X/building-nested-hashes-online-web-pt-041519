8275119b90cad2d1783039d31ae5e7528dbfa540
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-23 14:45:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
     :montague => {
     :patriarch => {
-      name: "Lord Monatague",
+      name: "Lord Montague",
       age: "53"
     },
     :matriarch => {
