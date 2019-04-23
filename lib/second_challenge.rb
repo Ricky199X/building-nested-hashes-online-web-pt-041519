0f8938358a1467d3488f6c392e4ac5f8fdb0b277
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-23 14:39:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,55 +2,39 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-        name: 
-        age: 
+       
       },
       :matriarch => {
-        name: 
-        age: 
+       
       },
       :hero => {
-        name: 
-        age: 
-        status: 
+         
       },
       :heros_friends => [
       {
-        name:
-        age:
-        attitude: 
+        
       },
       {
-        name:
-        age: 
-        attitude: 
+         
       }
     ]
   }, 
     :capulet => {
       :patriarch => {
-        name: 
-        age: 
+       
     },
       :matriarch => {
-        name: 
-        age: 
+       
     },
       :heroine => {
-      name: 
-      age:
-      status: 
+       
     },
       :heroines_friends => [
       {
-        name: 
-        age: 
-        attitude: 
+         
       }, 
       { 
-        name: 
-        age: 
-        attitude: 
+       
       }
     ]
   }
